fb7b3d413f65e886bc777e0c3f929ef49a21edfd
Li Tian <danieltian059@gmail.com>
2025-06-16 12:51:17 -0400

    Add initial project configuration files and update .gitignore

---
+++ fp-account/src/main/java/com/litian/account/FpAccountApplication.java
@@ -0,0 +1,13 @@
+package com.litian.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FpAccountApplication.class, args);
+	}
+
+}
---
+++ fp-content/src/test/java/com/litian/content/FpContentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.litian.content;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpContentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
