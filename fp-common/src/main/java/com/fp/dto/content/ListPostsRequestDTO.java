c0206673575afa639e132d8517e113fccc4ba4c4
Li Tian <danieltian059@gmail.com>
2025-12-08 12:07:41 -0500

    feat: Add ListPostsRequestDTO prepared for query params and dynamic query in the repository.

---
+++ fp-common/src/main/java/com/fp/dto/content/ListPostsRequestDTO.java
@@ -0,0 +1,32 @@
+package com.fp.dto.content;
+
+import jakarta.validation.constraints.Pattern;
+import lombok.Builder;
+import lombok.Data;
+///
+/// Request Params for listing posts.
+///
+@Data
+@Builder
+public class ListPostsRequestDTO {
+    // The ID of the account for which to list posts.
+    private String accountId;
+
+    // The number of posts to return for each page.
+    @Builder.Default
+    private Integer limit = 20;
+
+    /// Cursor for pagination, indicating the position to start fetching posts.
+    /// cursor format: createdAt Instant timestamp#postId
+    /// Example: 2025-06-29-11:50:3233545Z#19
+    private String cursor;
+
+    // The keyword to filter posts by content.
+    private String keyword;
+
+    // The status to filter posts (ACTIVE, DRAFT, HIDDEN).
+    @Pattern(regexp = "ACTIVE|DRAFT|HIDDEN", message = "Status must be 'ACTIVE', 'DRAFT', or 'HIDDEN'")
+    private String status;
+
+
+}
